a017e6148a8c00715ae71a310586084c8fe14611
DominicLGit <dominic.langsdale@gmail.com>
2023-04-26 10:54:51 +0100

    urls to re_path for django 4

--- wagtail_comments_xtd/urls.py
+++ wagtail_comments_xtd/urls.py
@@ -1,13 +1,12 @@
 from __future__ import absolute_import, unicode_literals
-from django.conf.urls import url
 from wagtail_comments_xtd import views
-
+from django.urls import re_path
 
 urlpatterns = [
-    url(r'^$', views.pages, name='wagtail_comments_xtd_pages'),
-    url(r'^(?P<pk>\d+)/$', views.comments, name='wagtail_comments_xtd_comments'),
-    url(r'^(?P<page_pk>\d+)/comment/(?P<comment_pk>\d+)/thread/$',
+    re_path(r'^$', views.pages, name='wagtail_comments_xtd_pages'),
+    re_path(r'^(?P<pk>\d+)/$', views.comments, name='wagtail_comments_xtd_comments'),
+    re_path(r'^(?P<page_pk>\d+)/comment/(?P<comment_pk>\d+)/thread/$',
         views.comment_thread, name='wagtail_comments_xtd_comment_thread'),
-    url(r'^(?P<page_pk>\d+)/comment/(?P<comment_pk>\d+)/update/(?P<action>publish|unpublish|hide|show)/$',
+    re_path(r'^(?P<page_pk>\d+)/comment/(?P<comment_pk>\d+)/update/(?P<action>publish|unpublish|hide|show)/$',
         views.update, name='wagtail_comments_xtd_publication'),
 ]
